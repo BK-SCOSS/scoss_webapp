43b3ae3d42119d746186a35c94300ac12cb3d611
tranvien98 <tranvanvien98bg@gmail.com>
2020-10-28 17:51:46 +0700

    fix api run contest

--- app/controllers/similarity_checker.py
+++ app/controllers/similarity_checker.py
@@ -32,8 +32,9 @@ def cal_scoss(sources, metrics):
 			else:
 				sc.add_source_str(source['source_str'], mask=source['mask'])
 			programs.append(sc)
-	for program in programs: 
-		for matrix in program.get_matches():
+	for program in programs:
+		print(program.get_matches(or_thresholds=True)) 
+		for matrix in program.get_matches(or_thresholds=True):
 			similarity_list.append(matrix)
 		for matrix in program.get_aligment_matrix():
 			alignment_list.append(matrix)
@@ -94,20 +95,20 @@ def run_problem(problem_id):
 	url_scoss = URL + '/api/problems/' + str(problem_id) + '/results/scoss'
 	url_smoss = URL + '/api/problems/' + str(problem_id) + '/results/smoss'
 	req_status = requests.get(url=url_status)
-	if req_status.json()['problem_status'] == 'init' or req_status.json()['problem_status'] == 'reopen':
+	if req_status.json()['problem_status'] in ['init', 'reopen', 'checked']:
 		doc_status = {
 			"problem_status": "waiting"
 		}
 		requests.put(url=url_status, json=doc_status)
 		similarity_list, alignment_list = cal_scoss(data_problem['sources'], data_problem['metrics'])
-		if len(similarity_list) > 0 and len(alignment_list) > 0:
+		if len(similarity_list) >= 0 and len(alignment_list) >= 0:
 			doc_scoss = {
 				"similarity_list": similarity_list,
 				"alignment_list": alignment_list
 			}
 			requests.put(url=url_scoss, json=doc_scoss)
 		similarity_smoss_list, alignment_smoss_list = cal_smoss(data_problem['sources'], data_problem['metrics'])
-		if len(similarity_smoss_list) > 0 and len(alignment_smoss_list) > 0:
+		if len(similarity_smoss_list) >= 0 and len(alignment_smoss_list) >= 0:
 			doc_scoss = {
 				"similarity_smoss_list": similarity_smoss_list,
 				"alignment_smoss_list": alignment_smoss_list
